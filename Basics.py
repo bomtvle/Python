82cd02269438036e9b3951d8df97b3185342d9f1
tvlecgu <tvle.cgu@gmail.com>
2019-06-02 21:57:32 +0800

    new

--- Basics.py
+++ Basics.py
@@ -152,3 +152,21 @@ def num_chk(n):
         print("Number fails condition")
 num_chk(12)
 num_chk(10)
+
+num = 45
+if num > 25:
+    print("Hurray! {} is greater than 25".format(num))
+
+num = 45
+if num % 2 == 0:
+    print("{} is an even number".format(num))
+else:
+    print("{} is an odd number".format(num))
+
+num = 0
+if num < 0:
+    print("{} is a negative number".format(num))
+elif num > 0:
+    print("{} is a positive number".format(num))
+else:
+    print("{} is neither postive nor a negative number".format(num))
