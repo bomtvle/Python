5fb301e9c0a784244c1f7dcf02786f555f6b028f
tvlecgu <tvle.cgu@gmail.com>
2019-06-02 23:06:37 +0800

    ok

--- Basics.py
+++ Basics.py
@@ -189,3 +189,15 @@ usr_string = 'not a number'
 while not usr_string.isnumeric():
     usr_string = input("Enter a positive integer: ")
 
+prev_num = 0
+curr_num = 0
+print("The first ten numbers in fibonacci sequence: ", end='')
+for num in range(10):
+    print(curr_num, end=' ')
+    if num == 0:
+        curr_num = 1
+        continue
+    temp = curr_num
+    curr_num = curr_num + prev_num
+    prev_num = temp
+print("")
--- tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-usr_string = 'not a number'
-while not usr_string.isnumeric():
-    usr_string = input("Enter a positive integer: ")
