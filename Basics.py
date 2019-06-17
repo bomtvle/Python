8e71ce3643fad0c798fea46faaf826ef4a2ee94f
Tuan-Vinh Le <tuan-vinhle@Tuan-Vinhs-MacBook-Air.local>
2019-06-17 23:14:22 +0800

    yes

--- Basics.py
+++ Basics.py
@@ -256,3 +256,27 @@ even = [2, 4, 6]
 for i, j in zip(odd, even):
     print(i + j)
 
+import time
+
+numbers = list(range(1,100001))
+fl_square_numbers = []
+t0 = time.perf_counter()
+for num in numbers:
+    fl_square_numbers.append(num * num)
+t1 = time.perf_counter()
+lc_square_numbers = [num * num for num in numbers]
+
+t2 = time.perf_counter()
+fl_time = t1 - t0
+lc_time = t2 - t1
+improvement = (fl_time - lc_time) / fl_time * 100
+
+print("Time with for loop:           {:.4f}".format(fl_time))
+print("Time with list comprehension: {:.4f}".format(lc_time))
+print("Improvement:                  {:.2f}%".format(improvement))
+
+if fl_square_numbers == lc_square_numbers:
+    print("\nfl_square_numbers and lc_square_numbers are equivalent")
+else:
+    print("\nfl_square_numbers and lc_square_numbers are NOT equivalent")
+    
